ffebd271797fe573db9910f21358a99c245901ee
antaeho <an981022@naver.com>
2023-02-23 09:35:41 +0900

    feat: team 기본 구현

--- src/main/java/hello/board/entity/Member.java
+++ src/main/java/hello/board/entity/Member.java
@@ -1,16 +1,28 @@
 package hello.board.entity;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
 @NoArgsConstructor
 public class Member {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
+
+    private String name;
+
+    private int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
