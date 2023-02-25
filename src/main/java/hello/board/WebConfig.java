273036debd1e85ddb7775ccfbb04062dc7e85d85
antaeho <an981022@naver.com>
2023-02-25 10:35:28 +0900

    feat: 로그인 필터 구현 + 권한 없을시 돌아가는 홈 구현

---
+++ src/main/java/hello/board/WebConfig.java
@@ -0,0 +1,23 @@
+package hello.board;
+
+import hello.board.filter.LoginCheckFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
