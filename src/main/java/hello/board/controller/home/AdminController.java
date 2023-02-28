316701430ce10249ed8637d0085bf68bead9e848
antaeho <an981022@naver.com>
2023-02-28 15:45:01 +0900

    feat: home / admin ResponseEntity 적용

--- src/main/java/hello/board/controller/home/AdminController.java
+++ src/main/java/hello/board/controller/home/AdminController.java
@@ -1,23 +1,23 @@
 package hello.board.controller.home;
 
+import hello.board.controller.member.dto.res.MemberResDto;
 import hello.board.controller.post.dto.res.PostResDto;
-import hello.board.domain.member.entity.Member;
 import hello.board.domain.member.service.MemberService;
 import hello.board.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
@@ -25,27 +25,31 @@ public class AdminController {
     private final MemberService memberService;
     private final PostService postService;
 
-    @GetMapping
-    public String adminHome() {
-        return "home/adminHome";
-    }
-
     //멤버 전체 조회 화면 메서드
     @GetMapping("/members")
-    public String findAll(Model model, @RequestParam("page") int page) {
+    public ResponseEntity<List<MemberResDto>> findAll(@RequestParam("page") int page) {
         PageRequest pageRequest = PageRequest.of(page, 10);
-        Page<Member> members = memberService.findAll(pageRequest);
-        model.addAttribute("members", members);
-        return "members/members";
+        List<MemberResDto> members = findAllMember(pageRequest);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(members);
     }
 
     //모든 게시글 화면 메서드
     @GetMapping("/posts")
-    public String findAllPost(Model model, @RequestParam("page") int page) {
+    public ResponseEntity<List<PostResDto>> findAllPost(@RequestParam("page") int page) {
         PageRequest pageRequest = PageRequest.of(page, 10);
         List<PostResDto> posts = findAllPost(pageRequest);
-        model.addAttribute("posts", posts);
-        return "posts/posts";
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(posts);
+    }
+
+    private List<MemberResDto> findAllMember(Pageable pageable) {
+        return memberService.findAll(pageable)
+                .stream()
+                .map(MemberResDto::new)
+                .collect(Collectors.toList());
     }
 
     private List<PostResDto> findAllPost(Pageable pageable) {
--- src/main/java/hello/board/controller/home/HomeController.java
+++ src/main/java/hello/board/controller/home/HomeController.java
@@ -2,24 +2,30 @@ package hello.board.controller.home;
 
 import hello.board.argumentresolver.Login;
 import hello.board.domain.member.entity.Member;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     //홈 화면 메서드
     @GetMapping("/home")
-    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
+    public ResponseEntity homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
 
         //세션에 회원 데이터가 없으면 home
         if (loginMember == null) {
-            return "home/home";
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .build();
         }
 
         //세션이 유지되면 로그인으로 이동
         model.addAttribute("member", loginMember);
-        return "home/loginHome";
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .build();
     }
 }
